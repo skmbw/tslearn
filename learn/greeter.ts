da1b245dde9867efb947ef29de2a42bd5c18348d
skmbw <tongku2008@126.com>
2019-09-06 10:37:44 +0800

    ts for in循环

--- learn/greeter.ts
+++ learn/greeter.ts
@@ -3,11 +3,12 @@ debugger; // ts-node的测试，使用node进行debug，不使用浏览器。使
 export class Programmer {
     name:string;
  
+    // F5开始调试，启动后，F5到下一个断点（IDEA F9），F10单步调试（IDEA F8）
     study() {
         console.log(this.name + "学习");
-        const fruits = new Set(['apple', 'pear', 'mango']);
-        // fruits['peach'] = 'Princess Peach.';
-        for (const fruit in fruits) {
+        const fruits = new Set(['apple', 'pear', 'mango']); // 只有es6以后才有Set
+        // fruits['peach'] = 'Princess Peach.'; // 这个用法是不对的
+        for (const fruit in fruits) { // 这种什么都没有
             console.log(fruit);
         }
 
