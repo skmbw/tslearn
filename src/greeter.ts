4a24cdadd5995c55d0c9a292fb31bbd8f197bac7
skmbw <tongku2008@126.com>
2019-09-06 16:24:30 +0800

    语法学习

--- src/greeter.ts
+++ src/greeter.ts
@@ -39,7 +39,7 @@ function sum2() {
         callee: Function;
     } = arguments;
 }
-// 可选参数
+// 可选参数，可选参数后面不允许再出现必需参数了
 function buildName(firstName: string, lastName?: string) {
     if (lastName) {
         return firstName + ' ' + lastName;
@@ -49,3 +49,37 @@ function buildName(firstName: string, lastName?: string) {
 }
 let tomcat = buildName('Tom', 'Cat');
 let tom = buildName('Tom');
+
+// 默认参数，会将添加了默认值的参数识别为可选参数，但是不必在必须参数的后面
+function buildName2(firstName: string, lastName: string = 'Cat') {
+    return firstName + ' ' + lastName;
+}
+let tomcat2 = buildName2('Tom', 'Cat');
+let tom2 = buildName2('Tom');
+
+// 剩余参数只能是最后一个参数
+function push(array: any[], ...items) { // items 隐含 any[] 类型，编辑器警告而已
+    items.forEach(function(item) {
+        array.push(item);
+    });
+}
+
+function push2(array: any[], ...items: any[]) { // items 隐含 any[] 类型
+    items.forEach(function(item) {
+        array.push(item);
+    });
+}
+
+let a: any[] = []; // a 隐含 any[] 类型
+push(a, 1, 2, 3);
+
+// 函数重载，匹配时，从上往下，精确匹配
+function reverse(x: number): number;
+function reverse(x: string): string;
+function reverse(x: number | string): number | string { // 联合类型
+    if (typeof x === 'number') {
+        return Number(x.toString().split('').reverse().join(''));
+    } else if (typeof x === 'string') {
+        return x.split('').reverse().join('');
+    }
+}
